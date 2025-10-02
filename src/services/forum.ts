5b43b948b332a3462cb6faf4741d4479888011fd
Metehan Kutay <metehankutay@gmail.com>
2025-10-02 21:29:41 +0300

    Localhost

--- src/services/forum.ts
+++ src/services/forum.ts
@@ -20,7 +20,7 @@ export type Question = {
   reply_count: number;
 };
 
-const API_URL = "http://localhost:5000";
+const API_URL = "http://127.0.0.1:5000";
 
 async function authHeaders(includeContent = false) {
   const session = await fetchAuthSession();
